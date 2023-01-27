9e1f8249985e73bbd18f3baaae027c66223645f9
Dorian Li <49279922+dorian-li@users.noreply.github.com>
2023-01-27 22:03:13 +0800

    fix: delete fpath setting

--- fnm.plugin.zsh
+++ fnm.plugin.zsh
@@ -5,5 +5,4 @@ eval "$(fnm env --use-on-cd)"
 
 # Add fnm completions
 fnm_completion="$(dirname "$(readlink -f "$0")")/_fnm"
-fpath=($fnm_completion $fpath)
 fnm completions --shell zsh > "$fnm_completion"
